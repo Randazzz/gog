df232679cd18203dbd160f6bcf7b45b63e66b9fc
Randazzz <kir.liaxiwsky2015@yandex.ru>
2024-11-08 17:32:06 +0300

    Добавлены тесты для страницы регистрации и входа

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,85 @@
-from django.test import TestCase
+from datetime import timedelta
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import now
+
+from apps.users.forms import UserLoginForm
+from apps.users.models import User, EmailVerification
+
+
+class UserRegistrationViewTests(TestCase):
+    fixtures = ['socialapp.json']
+
+    def setUp(self):
+        self.path = reverse('users:registration')
+        self.data = {
+            'username': 'testusername',
+            'email': 'testemail@yandex.ru',
+            'password1': 'Testpassword123',
+            'password2': 'Testpassword123',
+        }
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, template_name='users/register.html')
+        self.assertContains(response, 'Регистрация')
+
+    def test_user_registration_post(self):
+        username = self.data['username']
+        response = self.client.post(self.path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expires_at.date(),
+            (now() + timedelta(days=2)).date()
+        )
+
+    def test_user_registration_post_error(self):
+        User.objects.create(username=self.data['username'])
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
+
+
+class UserLoginViewTestCase(TestCase):
+    fixtures = ['socialapp.json']
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword'
+        )
+        self.login_url = reverse('users:login')
+
+    def test_login_page_loads_successfully(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertIsInstance(response.context['form'], UserLoginForm)
+
+    def test_login_successful(self):
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        response = self.client.post(self.login_url, data)
+        self.assertRedirects(response, reverse('index'))
+
+    def test_login_failure(self):
+        data = {
+            'username': 'testuser',
+            'password': 'wrongpassword'
+        }
+        response = self.client.post(self.login_url, data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertIn('form', response.context)
+        self.assertTrue(response.context['form'].errors)
